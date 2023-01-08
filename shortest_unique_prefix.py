033e07d5ee35945372124c3480e881dc3148e2a9
rickyjames1750 <rickyjames1750@gmail.com>
2023-01-08 14:36:41 -0800

    Added Code Skeleton

---
+++ shortest_unique_prefix.py
@@ -0,0 +1,18 @@
+"""
+
+Shortest unique prefix 
+
+Provided a list of words, for each word find the shortest unique prefix. 
+You can assume a word will not be a substring of another word
+(ie play and playing won't be in the same words list)
+
+Example
+Input: ['joma', 'john', 'jack', 'techlead']
+Output: ['jom', 'joh', 'ja', 't']
+
+"""
+
+def shortest_unique_prefix(words):
+
+print(shortest_unique_prefix(['joma', 'john', 'jack', 'techlead']))
+# ['jom', 'joh', 'ja', 't']
