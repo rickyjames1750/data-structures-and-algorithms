34976bb22484871fd9bd6f07eb54bba8599b9e36
rickyjames1750 <rickyjames1750@gmail.com>
2023-01-08 15:06:02 -0800

    Added Class Node & Class Trie

--- shortest_unique_prefix.py
+++ shortest_unique_prefix.py
@@ -12,6 +12,34 @@ Output: ['jom', 'joh', 'ja', 't']
 
 """
 
+class Node:
+    def __init__(self):
+        self.count = 0
+        self.children = {}
+
+class Trie: 
+    def __init__(self):
+        self.root = Node()
+    def insert(self, word):
+        curr_node = self.root
+        for c in word:
+            if c not in curr_node.children:
+                curr_node.children[c] = Node()
+            curr_node = curr_node.children[c]
+            curr_node.count += 1
+    def unique_prefix(self, word):
+        curr_node = self.root
+        prefix = ''
+
+        for c in word:
+            if curr_node.count == 1:
+                return prefix
+            else:
+                curr_node = curr_node.children[c]
+                prefix += c
+        return prefix
+
+    
 def shortest_unique_prefix(words):
 
 print(shortest_unique_prefix(['joma', 'john', 'jack', 'techlead']))
