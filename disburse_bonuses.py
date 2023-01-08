89f19f70c098f970249b5b75582460679bb51c12
rickyjames1750 <rickyjames1750@gmail.com>
2023-01-08 13:13:55 -0800

    Code Build Complete

--- disburse_bonuses.py
+++ disburse_bonuses.py
@@ -21,6 +21,25 @@ Here's the starting point:
 """
 
 def getBonuses(performance):
+    # Count how many workers there are 
+    count = len(performance)
+    # Bonus array and init 1 
+    bonus = [1] * count
+
+    # Now lets go from left to right 
+    # Start at 1 becuase if we start at 0 we are not going to have anyone to our left 
+    for i in range(1, count):
+        # if my performance is bigger than the guy on my left then I get a bonus 
+        if performance[i-1] < performance[i]:
+            bonus[i] = bonus[i-1] + 1
+
+    # Don't want to go all the way to the right or else we go out of index
+    # - 1 -1 since we are going backwards 
+    for i in range(count - 2, -1, -1):
+        if performance[i+1] < performance[i]:
+            bonus[i] = max(bonus[i], bonus[i + 1] + 1)
+
+    return bonus
 
 print(getBonuses([1, 2, 3, 2, 3, 5, 1]))
 # [1, 2, 3, 1, 2, 3, 1]
