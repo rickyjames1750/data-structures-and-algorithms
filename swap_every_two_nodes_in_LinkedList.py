9f2c38a213dbfee374729c04e16ffb1d0db2280e
rickyjames1750 <rickyjames1750@gmail.com>
2023-01-08 13:40:45 -0800

    Code Build Completed

--- swap_every_two_nodes_in_LinkedList.py
+++ swap_every_two_nodes_in_LinkedList.py
@@ -17,6 +17,17 @@ class Node:
         return f"{self.value}, ({self.next.__repr__()})"
     
 def swap_every_two(llist):
+    # Not really necessary more for aesthetic 
+    head = llist
+
+    # Initialize our current pointer to the head
+    curr = head
+    # Iterate through the list
+    while curr is not None and curr.next is not None:
+        curr.value, curr.next.value = curr.next.value, curr.value
+        curr = curr.next.next
+
+    return head
 
 llist = Node(1, Node(2, Node(3, Node(4, Node(5)))))
 print(swap_every_two(llist))
