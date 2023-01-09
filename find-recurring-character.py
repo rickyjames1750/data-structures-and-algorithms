c253d54cb2dc725e1bc0e478df0130c26364c2d7
rickyjames1750 <rickyjames1750@gmail.com>
2023-01-09 09:58:22 -0800

    Added Skeleton Code

---
+++ find-recurring-character.py
@@ -0,0 +1,24 @@
+"""
+Find reccurring character 
+
+Provided  a string, return the first recurring letter that appears. If there are no recurring letters, return None.
+
+Examle below:
+
+Example: 
+
+Input: qwertty
+Output: t
+
+Input: qwerty
+Output: None 
+
+"""
+
+def first_recurring_char(s):
+
+print(first_recurring_char("qwertty"))
+# t
+
+print(first_recurring_char("qwerty"))
+# None 
