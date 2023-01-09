20a0d9c3c41fd71432940569b1019a3c17656fff
rickyjames1750 <rickyjames1750@gmail.com>
2023-01-09 09:26:44 -0800

    Completes Code Build

--- find_subtree.py
+++ find_subtree.py
@@ -18,10 +18,25 @@ class Node:
         return f"(Value: {self.value} Left: {self.left} Right: {self.right})"
 
 def find_subtree(s, t):
-    t3 = Node(4, Node(3), Node(2))
-    t2 = Node(5, Node(4), Node(-1))
 
-    s = Node(4, Node(3), Node(2))
+    def preorder_traversal(node):
+        if node is None:
+            return ''
+        return str(node.value) + '-' + preorder_traversal(node.left) + '-' + preorder_traversal(node.right)
+
+    return preorder_traversal(s) in preorder_traversal(t)   
+    
+    
+    
+    
+    
+    
+    
+    
+t3 = Node(4, Node(3), Node(2))
+t2 = Node(5, Node(4), Node(-1))
+
+s = Node(4, Node(3), Node(2))
 
 """
     Tree t: 
