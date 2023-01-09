ea5b8bad86f7a4372dbe821a376f872f9b62beb8
rickyjames1750 <rickyjames1750@gmail.com>
2023-01-08 16:05:30 -0800

    Added Skeleton Code

---
+++ running_median.py
@@ -0,0 +1,15 @@
+"""
+Running Median
+
+You are given a stream of numbers. Compute the median for each new element. 
+
+Eg. Given [2, ,1, 4, 7, 2, 0, 5], the alrogithm should output [2, 1.5, 2, 3.0, 2, 2, 2]
+
+Here's a starting point:
+
+"""
+
+def running_median(stream):
+
+running_median([2, 1, 4, 7, 2, 0, 5])
+# 2 1.5 2 3.0 2 2.0 2
