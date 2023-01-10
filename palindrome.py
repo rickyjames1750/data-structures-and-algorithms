bead504664cbddeeaee08f41f83968e1275e6d5e
rickyjames1750 <rickyjames1750@gmail.com>
2023-01-10 13:22:04 -0800

    added skeleton code

---
+++ palindrome.py
@@ -0,0 +1,18 @@
+"""
+
+Check for palindrome 
+
+Provided a string, determine if there is a way to arrange the string
+such that the string is a palidrome. If such arrangement exists, return
+a palidrome (There could be many arrangments).
+
+Otherwise return None.
+
+"""
+
+
+def find_palindrome(s):
+
+
+print(find_palindrome('momom'))
+# momom
