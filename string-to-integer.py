8f9e7fe166edb88e263e351bc7a0ba33e59b2ff8
rickyjames1750 <rickyjames1750@gmail.com>
2023-01-10 08:56:54 -0800

    Added Skeleton Code

---
+++ string-to-integer.py
@@ -0,0 +1,15 @@
+"""
+Provided a string, convert it to an integer without using 
+the bulletin str function. You are allowed to use ord to convert
+a character to ASCII code. 
+
+Consider all possible cases of an integer. In the case where the string
+is not a valid integer, return None. 
+
+"""
+
+def convert_to_int(s):
+    
+
+print(convert_to_int('-105') + 1)
+# -104
