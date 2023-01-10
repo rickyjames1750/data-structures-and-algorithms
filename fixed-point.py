181d0246a0b355dc5c7ad365077f5949436a3d6d
rickyjames1750 <rickyjames1750@gmail.com>
2023-01-09 17:30:03 -0800

    Added Skeleton Code

---
+++ fixed-point.py
@@ -0,0 +1,17 @@
+"""
+Fixed Point
+
+A fixed point in a list is where the value is equal to its index. 
+So for example the list [-5, 1, 3, 4], 1 is a fixed point in the 
+list since the index and value is the same. Find a fixed point
+(there can be many, just return 1) in a sorted list of distinct
+elements, or return None if it doesn't exist. 
+
+Here is a starting point: 
+
+"""
+
+def find_fixed_point(nums):
+
+print find_fixed_point([-5, 1, 3, 4])
+# 1
