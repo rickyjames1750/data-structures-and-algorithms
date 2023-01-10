721eb212d261515e377bc05e9ebe3d74a18746da
rickyjames1750 <rickyjames1750@gmail.com>
2023-01-09 16:33:13 -0800

    Added Code Skeleton

---
+++ make-the-largest-number.py
@@ -0,0 +1,15 @@
+"""
+Make the largest Number 
+
+Provided a number of integers, combine them so it would create the largest number. 
+
+Example:
+Input: [17, 7, 2, 45, 72]
+Output: 77245217
+
+"""
+
+def largestNum(nums):
+
+print largestNum([17, 7, 45, 72])
+# 77245217
