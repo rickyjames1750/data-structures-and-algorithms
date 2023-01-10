c728a5e04ce5b65473cf9d310c36a9e826200790
rickyjames1750 <rickyjames1750@gmail.com>
2023-01-10 13:49:26 -0800

    added palindrome function

--- palindrome.py
+++ palindrome.py
@@ -9,10 +9,32 @@ a palidrome (There could be many arrangments).
 Otherwise return None.
 
 """
-
+from collections import defaultdict
 
 def find_palindrome(s):
+    char_count = defaultdict(int)
+
+    for c in s:
+        char_count[c] += 1
+
+    odd_char = ""
+    palindrome = ""
+
+    for c, cnt in char_count.items():
+        if cnt % 2 == 0:
+            palindrome += c * (cnt//2)
+        elif not odd_char:
+            odd_char = c
+            palindrome += c * (cnt//2)
+        else:
+            return None
+
+    return palindrome + odd_char + palindrome[::-1]
 
 
 print(find_palindrome('momom'))
-# momom
+# mommom
+print(find_palindrome('qqaaaao'))
+# None
+
+
