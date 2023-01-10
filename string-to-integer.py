11ed94cf52e37e84c07e4df18791ef557f414278
rickyjames1750 <rickyjames1750@gmail.com>
2023-01-10 09:22:11 -0800

    added conversion to int

--- string-to-integer.py
+++ string-to-integer.py
@@ -9,7 +9,25 @@ is not a valid integer, return None.
 """
 
 def convert_to_int(s):
-    
+    if not s:
+        return None
+
+    result = 0
+    is_negative = False
+
+    if s[0] == '-':
+        is_negative = True 
+        s = s[1:]
+
+        ascii_zero = ord('0')
+
+        for ch in s:
+            if not ch.isdigit():
+                return None
+            result = result * 10 + ord(ch) - ascii_zero # ord('0')
+
+        return -result if is_negative else result
 
 print(convert_to_int('-105') + 1)
+print(convert_to_int('103453455'))
 # -104
