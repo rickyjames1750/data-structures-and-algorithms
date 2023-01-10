44598b66d1f02ee9b42adec248c7a18aa43ef4e5
rickyjames1750 <rickyjames1750@gmail.com>
2023-01-09 17:17:15 -0800

    Added stack[] array

--- valid-parentheses.py
+++ valid-parentheses.py
@@ -1,3 +1,35 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        
+        stack = []
+        for c in s:
+            if c == '(':
+                stack.append('(')
+            if c == ')':
+                if len(stack) == 0:
+                    return False
+                if stack[-1] != '(':
+                    return False
+                else: stack.pop()
+
+            if c == '{':
+                stack.append('{')
+            if c == '}':
+                if len(stack) == 0:
+                    return False
+                if stack[-1] != '{':
+                    return False
+                else:
+                    stack.pop()
+                if c == '[':
+                    stack.append(']')
+                if c == ']':
+                    if len(stack) == 0:
+                        return False
+                    if stack[-1] != ']':
+                        return False
+                    else:
+                        stack.pop()
+                if len(stack) > 0:
+                    return False
+                else: 
+                    return True
