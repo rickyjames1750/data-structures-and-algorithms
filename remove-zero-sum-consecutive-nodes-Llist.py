b595613fdff5335893848b4693e70f678d532288
rickyjames1750 <rickyjames1750@gmail.com>
2023-01-10 14:20:06 -0800

    added removeZeroSumSublists function

--- remove-zero-sum-consecutive-nodes-Llist.py
+++ remove-zero-sum-consecutive-nodes-Llist.py
@@ -6,3 +6,18 @@
 
 class  Solution:
     def removeZeroSumSublists(self, head: ListNode) -> ListNode: 
+        curr = dummy = ListNode(0)
+        dummy.next = head 
+        prefix = 0 
+        seen = collections.OrderedDict()
+        while curr: 
+            prefix += curr.val
+            if prefix not in seen:
+                seen[prefix] = curr
+            else:
+                node = seen[prefix]
+                node.next = curr.next
+                while list(seen.keys())[-1] != prefix:
+                    seen.popitem()
+                curr = curr.next
+        return dummy.next
