8cc3948e74a401b15995fb3df67e0b0d24ae927d
rickyjames1750 <rickyjames1750@gmail.com>
2023-01-09 16:49:41 -0800

    Completes Code Build

--- make-the-largest-number.py
+++ make-the-largest-number.py
@@ -9,7 +9,20 @@ Output: 77245217
 
 """
 
-def largestNum(nums):
+from functools import cmp_to_key
 
-print largestNum([17, 7, 45, 72])
+def largestNum(nums):
+    def compare(a, b):
+        if str(a) + str(b) < str(b) + str(a):
+            return 1
+        else:
+            return -1
+
+        str_nums = [str(n) for n in sorted(nums, key=cmp_to_key(compare))]
+        return "".join(str_nums)
+
+
+print(largestNum([17, 7, 45, 72]))
+print(largestNum([17, 7, 2, 45, 78]))
+print(largestNum([97, 7, 2, 45, 78]))
 # 77245217
