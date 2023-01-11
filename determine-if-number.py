c862e718f04a29de6d4ed24efffb9871c6b4289f
rickyjames1750 <rickyjames1750@gmail.com>
2023-01-10 16:15:15 -0800

    added digit state class

--- determine-if-number.py
+++ determine-if-number.py
@@ -22,3 +22,39 @@ with int() or any other python functions.
 
 """
 
+from enum import Enum
+
+class DigitState(Enum):
+    BEGIN = 0
+    NEGATIVE1 = 1
+    DIGIT1 = 2
+    DOT = 3
+    DIGIT2 = 4
+    E = 5
+    NEGATIVE2 = 6
+    DIGIT3 = 7
+
+    fsm = {
+        DigitState.BEGIN: [DigitState.NEGATIVE1, DigitState.DIGIT1],
+        DigitState.NEGATIVE1: [DigitState.DIGIT1, DigitState.DOT],
+        DigitState.DIGIT1: [DigitState.DIGIT1, DigitState.DOT, DigitState.E],
+        DigitState.DOT: [DigitState.DIGIT2],
+        DigitState.DIGIT2: [DigitState.DIGIT2, DigitState.E],
+        DigitState.E: [DigitState.DIGIT3, DigitState.NEGATIVE2],
+        DigitState.NEGATIVE2: [DigitState.DIGIT3],
+        DigitState.DIGIT3: [DigitState.DIGIT3]
+    }
+
+    digit_lambdas = {
+        DigitState.BEGIN: lambda x: True,
+        DigitState.NEGATIVE1: lambda x: x == '-',
+        DigitState.DIGIT1:lambda x: x.isdigit(),
+        DigitState.DOT: lambda x: x == '.',
+        DigitState.DIGIT2: lambda x: x.isdigit(),
+        DigitState.E: lambda x: x == 'e',
+        DigitState.NEGATIVE2: lambda x: x == '-',
+        DigitState.DIGIT3: lambda x: x.isdigit()
+
+    }
+
+    
