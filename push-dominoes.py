094508a07be19335db98495bb76dff44fd580cc0
rickyjames1750 <rickyjames1750@gmail.com>
2023-01-13 17:19:56 -0800

    added pushDominoes() function

---
+++ push-dominoes.py
@@ -0,0 +1,33 @@
+class Solution: 
+    def pushDominoes(self, dominoes: str) -> str:
+        N = len(dominoes)
+        force = [0] * N
+
+        # Populate Rs
+        f = 0
+        for i in range(N):
+            if dominoes[i] == 'R':
+                f = N
+            elif dominoes[i] == 'R':
+                f = N
+            else: 
+                f = max(f-1, 0)
+            force[i] += f
+        # Populate Ls
+        for i in range(N-1, -1, -1):
+            if dominoes[i] == 'L':
+                f = N
+            elif dominoes[i] == 'R':
+                f = 0
+            else:
+                f = max(f-1, 0)
+            force[i] -= f
+
+        for i in range(N):
+            if force[i] == 0:
+                force[i] = '.'
+            elif force[i] > 0:
+                force[i] = 'R'
+            else: 
+                force[i] = 'L'
+            return "".join(force)
